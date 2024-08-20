290338e798ffc82dc6577f7644b875cd41cb0ee7
WIINNERz <121823374+WIINNERz@users.noreply.github.com>
2024-08-20 19:13:01 +0700

    Pull

--- src/utils/common.js
+++
@@ -1,8 +0,0 @@
-export const getRoomId = (userId1,userId2) =>{
-    const sortdIds = [userId1,userId2].sort();
-    const roomId = sortdIds.join('_');
-    return roomId;
-}
-export const getPetId = (petId) =>{
-    return petId;
-}
