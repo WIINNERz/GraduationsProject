ec85ee595a8c3596ae5daae5f0eeb422c094df37
WIINNER <121823374+WIINNERz@users.noreply.github.com>
2024-08-27 01:23:16 +0700

    Fixed Chat,Icon

--- src/configs/firebaseConfig.js
+++ src/configs/firebaseConfig.js
@@ -13,25 +13,16 @@ const firebaseConfig = {
   appId: "1:452497966669:web:642f2118703c5050653e04"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Auth with persistence
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
-
-// Optionally, if you need to use `getAuth` for other purposes, you can do so like this:
-const authInstance = getAuth(app);
-
-// Initialize Firestore and Storage
 const firestore = getFirestore(app);
 const storage = getStorage(app);
 
-// References to collections
-export const usersRef = collection(firestore, 'Users');
-export const petsRef = collection(firestore, 'Pets');
-export const roomRef = collection(firestore, 'Rooms');
+export const db = getFirestore(app);
+export const usersRef = collection(db, 'Users');
+export const petsRef = collection(db, 'Pets');
+export const roomRef = collection(db, 'Rooms');
 
-// Export services for use in other parts of your app
 export { auth, firestore, storage };
